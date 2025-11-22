432480e08edcb170b9e7fee793904a41f45104c5
Panaite Cristian-Florin <panaite.cristian99@gmail.com>
2025-11-22 18:34:51 +0200

    da

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,6 +1,8 @@
 import { MetadataRoute } from 'next';
 import { getAllRecipes, getAllCategories } from '@/lib/recipes';
 
+export const dynamic = 'force-static';
+
 export default function sitemap(): MetadataRoute.Sitemap {
   const baseUrl = 'https://recipe-master.vercel.app';
   const recipes = getAllRecipes();
